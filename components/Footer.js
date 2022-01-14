e5e6187b26a620b7d1e1417d650621f3bfb8d833
Bandit Silachai <mbandit001@yahoo.co.th>
2022-01-14 16:28:43 +0700

    small font size

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
         <footer>
             <div tw=" border-t border-gray-50 py-6 ">
                 <Container>
-                    <div tw="flex justify-between text-gray-600 ">
-                        <p> © 2022 Clean. All Rights Reserved. </p>
+                    <div tw="flex justify-between text-gray-600  ">
+                        <p tw="text-sm"> © 2022 Clean. All Rights Reserved. </p>
                         <p>Follow us:</p>
                     </div>
                 </Container>
