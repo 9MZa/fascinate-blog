f374280f9a74e41d78e710d173d653fb29611d45
Bandit Silachai <mbandit001@yahoo.co.th>
2022-01-16 19:00:29 +0700

    remove image

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Hero from "@/components/Section/Hero";
 import { Heading, Text } from "@/components/Elements/Title";
 import tw from "twin.macro";
-import People from "images/people.svg";
+
 
 const Title = "Contact us";
 const Detail = "Proident cupidatat commodo qui eiusmod irure. Cillum quis elit et do fugiat sit mollit proident magna culpa.";
@@ -15,11 +15,6 @@ const Contact = () => {
                     <div tw="col-span-1">
                         <Heading color="white" size={1}>{Title} </Heading>
                         <Text color="white" size={2}> {Detail} </Text>
-                    </div>
-                    <div tw="col-span-1">
-                        IMAGEsss
-                        <Image src={People} width={500} height={500} />
-
                     </div>
                 </div>
             </Hero>
