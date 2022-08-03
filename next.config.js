1a0494e978a9751549d3d584335bc327ec638493
Bandit Silachai <mbandit001@yahoo.co.th>
2022-08-03 12:52:14 +0700

    🐛 fix image problem

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["assets.website-files.com", "media.graphcms.com", "media.graphassets.com"],
+    dangerouslyAllowSVG: true,
   }
 };
